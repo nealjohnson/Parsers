9e096ec17c662427a13ec1bb1bdce7ffb9d5874c
nealjohnson <Anil.kumar.ait09@gmail.com>
2014-05-26 01:22:14 +0530

    initial

---
+++ automaticResumeParser/entity/ResumeSaveEntity.java
@@ -0,0 +1,376 @@
+package automaticResumeParser.entity;
+
+
+public class ResumeSaveEntity {
+ 
+private String name;
+ 
+private String address;
+ 	
+ 
+private String emailId;
+ 
+private String mobileNo;
+ 
+private String sex;
+ 
+private String relationShip;
+ 
+private String pinCode;
+ 
+private String state;
+ 
+private String cityOrDist;
+ 
+private String basicProfessionalQualification;
+ 
+private String mastersProfessionalQualification;
+ 
+private String basicProfessionalPercentage;
+ 
+private String mastersProfessionalPercentage;
+ 
+private String BasicProfessionalSpec;
+ 
+private String mastersProfessionalSpec;
+ 
+private String basicProfessionalInstitute;
+ 
+private String mastersProfessionalInstitute;
+ 
+private String mastersProfessionalPassingYear;
+ 
+private String basicProfessionalPassingYear;
+ 
+private String currentEmployer;
+ 
+private String currentDesignation;
+ 
+private String prevEmployer;
+ 
+private String prevDesignation;
+ 
+private String totalExperience;
+ 
+private String industry;
+ 
+private String functionalArea;
+ 
+private String ssc;
+ 
+private String sscInstitute;
+ 
+private String sscYear;
+ 
+private String hsc;
+ 
+private String hscInstitute;
+ 
+private String hscYear;
+ 
+private String insertionDate;
+ 
+private String mailIdFlag;
+ 
+private String exception;
+
+public String getSex() {
+	return sex;
+}
+
+public void setSex(String sex) {
+	this.sex = sex;
+}
+
+public String getSscInstitute() {
+	return sscInstitute;
+}
+
+public void setSscInstitute(String sscInstitute) {
+	this.sscInstitute = sscInstitute;
+}
+
+public String getSscYear() {
+	return sscYear;
+}
+
+public void setSscYear(String sscYear) {
+	this.sscYear = sscYear;
+}
+
+public String getHsc() {
+	return hsc;
+}
+
+public void setHsc(String hsc) {
+	this.hsc = hsc;
+}
+
+public String getHscInstitute() {
+	return hscInstitute;
+}
+
+public void setHscInstitute(String hscInstitute) {
+	this.hscInstitute = hscInstitute;
+}
+
+public String getHscYear() {
+	return hscYear;
+}
+
+public void setHscYear(String hscYear) {
+	this.hscYear = hscYear;
+}
+
+public String getBasicProfessionalQualification() {
+	return basicProfessionalQualification;
+}
+
+public void setBasicProfessionalQualification(
+		String basicProfessionalQualification) {
+	this.basicProfessionalQualification = basicProfessionalQualification;
+}
+
+public String getMastersProfessionalQualification() {
+	return mastersProfessionalQualification;
+}
+
+public void setMastersProfessionalQualification(
+		String mastersProfessionalQualification) {
+	this.mastersProfessionalQualification = mastersProfessionalQualification;
+}
+
+public String getBasicProfessionalSpec() {
+	return BasicProfessionalSpec;
+}
+
+public void setBasicProfessionalSpec(String basicProfessionalSpec) {
+	BasicProfessionalSpec = basicProfessionalSpec;
+}
+
+public String getMastersProfessionalSpec() {
+	return mastersProfessionalSpec;
+}
+
+public void setMastersProfessionalSpec(String mastersProfessionalSpec) {
+	this.mastersProfessionalSpec = mastersProfessionalSpec;
+}
+
+public String getBasicProfessionalInstitute() {
+	return basicProfessionalInstitute;
+}
+
+public void setBasicProfessionalInstitute(String basicProfessionalInstitute) {
+	this.basicProfessionalInstitute = basicProfessionalInstitute;
+}
+
+public String getCurrentEmployer() {
+	return currentEmployer;
+}
+
+public void setCurrentEmployer(String currentEmployer) {
+	this.currentEmployer = currentEmployer;
+}
+
+public String getCurrentDesignation() {
+	return currentDesignation;
+}
+
+public void setCurrentDesignation(String currentDesignation) {
+	this.currentDesignation = currentDesignation;
+}
+
+public String getPrevEmployer() {
+	return prevEmployer;
+}
+
+public void setPrevEmployer(String prevEmployer) {
+	this.prevEmployer = prevEmployer;
+}
+
+public String getPrevDesignation() {
+	return prevDesignation;
+}
+
+public void setPrevDesignation(String prevDesignation) {
+	this.prevDesignation = prevDesignation;
+}
+
+public String getTotalExperience() {
+	return totalExperience;
+}
+
+public String getBasicProfessionalPercentage() {
+	return basicProfessionalPercentage;
+}
+
+public void setBasicProfessionalPercentage(String basicProfessionalPercentage) {
+	this.basicProfessionalPercentage = basicProfessionalPercentage;
+}
+
+public String getMastersProfessionalPercentage() {
+	return mastersProfessionalPercentage;
+}
+
+public void setMastersProfessionalPercentage(
+		String mastersProfessionalPercentage) {
+	this.mastersProfessionalPercentage = mastersProfessionalPercentage;
+}
+
+public void setTotalExperience(String totalExperience) {
+	this.totalExperience = totalExperience;
+}
+
+public String getIndustry() {
+	return industry;
+}
+
+public void setIndustry(String industry) {
+	this.industry = industry;
+}
+
+public String getFunctionalArea() {
+	return functionalArea;
+}
+
+public void setFunctionalArea(String functionalArea) {
+	this.functionalArea = functionalArea;
+}
+
+
+
+
+
+
+
+
+
+public String getMailIdFlag() {
+	return mailIdFlag;
+}
+
+public void setMailIdFlag(String mailIdFlag) {
+	this.mailIdFlag = mailIdFlag;
+}
+
+
+
+
+
+public String getInsertionDate() {
+	return insertionDate;
+}
+
+public void setInsertionDate(String insertionDate) {
+	this.insertionDate = insertionDate;
+}
+
+public String getSsc() {
+	return ssc;
+}
+
+public void setSsc(String ssc) {
+	this.ssc = ssc;
+}
+
+
+public String getPinCode() {
+	return pinCode;
+}
+
+public void setPinCode(String pinCode) {
+	this.pinCode = pinCode;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public String getEmailId() {
+	return emailId;
+}
+
+public void setEmailId(String emailId) {
+	this.emailId = emailId;
+}
+
+public String getMobileNo() {
+	return mobileNo;
+}
+
+public void setMobileNo(String mobileNo) {
+	this.mobileNo = mobileNo;
+}
+
+
+
+public String getRelationShip() {
+	return relationShip;
+}
+
+public void setRelationShip(String relationShip) {
+	this.relationShip = relationShip;
+}
+public String getMastersProfessionalInstitute() {
+	return mastersProfessionalInstitute;
+}
+
+public void setMastersProfessionalInstitute(String mastersProfessionalInstitute) {
+	this.mastersProfessionalInstitute = mastersProfessionalInstitute;
+}
+
+public String getMastersProfessionalPassingYear() {
+	return mastersProfessionalPassingYear;
+}
+
+public void setMastersProfessionalPassingYear(
+		String mastersProfessionalPassingYear) {
+	this.mastersProfessionalPassingYear = mastersProfessionalPassingYear;
+}
+
+public String getBasicProfessionalPassingYear() {
+	return basicProfessionalPassingYear;
+}
+
+public void setBasicProfessionalPassingYear(String basicProfessionalPassingYear) {
+	this.basicProfessionalPassingYear = basicProfessionalPassingYear;
+}
+
+public String getState() {
+	return state;
+}
+
+public void setState(String state) {
+	this.state = state;
+}
+
+public String getCityOrDist() {
+	return cityOrDist;
+}
+
+public void setCityOrDist(String cityOrDist) {
+	this.cityOrDist = cityOrDist;
+}
+
+public String getException() {
+	return exception;
+}
+
+public void setException(String exception) {
+	this.exception = exception;
+}
+
+
+}
