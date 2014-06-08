f7fd183bfe6a5c1cb4b3286e838cab793c3cc771
Anil Sharma <Anil.Kumar.ait09@gmail.com>
2014-06-09 00:51:48 +0530

    Update ParsePersonalDetailsService.java

--- automaticResumeParser/service/ParsePersonalDetailsService.java
+++ automaticResumeParser/service/ParsePersonalDetailsService.java
@@ -3,18 +3,14 @@ package automaticResumeParser.service;
 import java.util.List;
 
 import automaticResumeParser.entity.ResumeSaveEntity;
-import automaticResumeParser.execption.ParserException;
-import automaticResumeParser.service.personal.ParseAddressService;
 import automaticResumeParser.service.personal.ParseEmailIdService;
 import automaticResumeParser.service.personal.ParseGenderOrMaritalService;
 import automaticResumeParser.service.personal.ParseNameService;
-import automaticResumeParser.service.personal.ParsePinOrPhoneService;
-import automaticResumeParser.utilities.AddressUtility;
 
 public class ParsePersonalDetailsService {
 
 	public ResumeSaveEntity parsePersonalDetails(
-			ResumeSaveEntity resumeSaveEntity, List<String> wordsList) throws ParserException {
+			ResumeSaveEntity resumeSaveEntity, List<String> wordsList) {
 
 		// ########################## GENDER AND MARITAL STATUS
 		// ####################################
@@ -29,23 +25,7 @@ public class ParsePersonalDetailsService {
 		// ########################## NAME ####################################
 		ParseNameService nameService = new ParseNameService();
 		resumeSaveEntity = nameService.parseName(wordsList, resumeSaveEntity);
-		System.out.println(resumeSaveEntity.getSex());
-		System.out.println(resumeSaveEntity.getEmailId());
-		// ############################ PINCODE OR PHONE NUMBER
-		// ##########################################
-		ParsePinOrPhoneService pinOrPhoneService = new ParsePinOrPhoneService();
-		// resumeSaveEntity = pinOrPhoneService.parsePinCodeOrPhoneNo(wordsList,
-		// resumeSaveEntity);
-		// ######################## FIND STATE
-		// ##################################
-		if (resumeSaveEntity.getPinCode() != null) {
-			resumeSaveEntity.setState(AddressUtility.FindState(Integer
-					.parseInt(resumeSaveEntity.getPinCode())));
-		}
-		// ######################################################################
-		ParseAddressService addressService = new ParseAddressService();
-		resumeSaveEntity = addressService.parseAddress(resumeSaveEntity,
-				wordsList);
+	
 		return resumeSaveEntity;
 	}
 }
