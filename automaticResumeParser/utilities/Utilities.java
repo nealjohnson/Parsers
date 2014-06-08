2f899221089ff9fc120e2565e77fb1dd3101efbd
Anil Sharma <Anil.Kumar.ait09@gmail.com>
2014-06-09 00:57:01 +0530

    Update Utilities.java

--- automaticResumeParser/utilities/Utilities.java
+++ automaticResumeParser/utilities/Utilities.java
@@ -1,9 +1,6 @@
 package automaticResumeParser.utilities;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
